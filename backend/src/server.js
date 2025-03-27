c8baf8be21126ca801c65168ffe05c0fce810fd0
NavadeepRangoni <rangoninavadeep@gmail.com>
2025-03-27 15:05:22 +0530

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -124,7 +124,7 @@ app.post("/api/users/login", async (req, res) => {
 });
 
 // ✅ 9. Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
